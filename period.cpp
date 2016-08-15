78897ec818b2b4d0febeb4b8a253c54778e63035
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-08-16 00:41:25 +0530

    finding the period

--- period.cpp
+++ period.cpp
@@ -2,13 +2,22 @@
 #include <fstream>
 #include <ctime>
 using namespace std;
-void main()
+int main()
 {
 	fstream o;
 	o.open("period.bin",ios::out | ios::app | ios::binary);
-	char arr[1000];
-	for (int i = 0; i < 100; ++i) ////can be modified for all numbers...
+	int f=0,s=1,next;
+	for (int n = 1; n <=(6*100)+1; ++n) ////can be modified for all numbers...
 	{
-		
+    	next=(f+s)%100;
+    	f=s;
+    	s=next;
+    	o.write((char*)&next,sizeof(next));
+    	if (n>1&&f==0&&s==1)
+    	{
+    			cout<<n;
+    			break;
+    	}	
 	}
+	o.close();
 }

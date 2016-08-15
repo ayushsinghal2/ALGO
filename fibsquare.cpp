11c909c1d0733a7fc927073e10c8b3db876b86ee
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-08-16 00:13:14 +0530

    Final time log^2(n)

--- fibsquare.cpp
+++ fibsquare.cpp
@@ -5,48 +5,46 @@ using namespace std;
 int main()
 {
 	int start = clock();
-	int a[2][2] = {{1,1},{1,0}};
-	int res[2][2] = {1,1,1,1};
+	int x[2][2] = {{1,1},{1,0}};
+	int y[2][2] = {1,0,0,1};
 	long long int n;
 	cin>>n;
-	while(n>0)
+	while(n!=0)
 	{
-		if((n%2)==0)
-		{
-			int D[2][2];
-			for(int R=0;R<2;R++)
-   			for(int C=0;C<2;C++)
-   			{
-      			D[R][C]=0;
-      			for(int T=0;T<2;T++)
-        			D[R][C]+=res[R][T]*res[T][C];
-    		}
-    	for(int i=0;i<2;i++)
-    		for (int j = 0; j < 2; ++j)
-    		{
-    			res[i][j]=D[i][j]%100;
-    			a[i][j]=res[i][j];
-    		}
-		}
-		else 
-		{
-			int D[2][2];
-			for(int R=0;R<2;R++)
-   				for(int C=0;C<2;C++)
+		int temp[2][2];
+		if(n%2!=0)
+		{	
+			for(int i=0;i<2;i++)
+   				for(int j=0;j<2;j++)
    				{
-      				D[R][C]=0;
-      				for(int T=0;T<2;T++)
-        				D[R][C]+=res[R][T]*a[T][C];
+      				temp[i][j]=0;
+      				for(int k=0;k<2;k++)
+        				temp[i][j]+=y[i][k]*x[k][j];
     			}
     		for(int i=0;i<2;i++)
     			for (int j = 0; j < 2; ++j)
     			{
-    				res[i][j]=D[i][j]%100;
+    				y[i][j]=temp[i][j]%100;
     			}
 		}
-		n=n/2;	
+		for(int i=0;i<2;i++)
+   			for(int j=0;j<2;j++)
+   			{
+      			temp[i][j]=0;
+      			for(int k=0;k<2;k++)
+        			temp[i][j]+=x[i][k]*x[k][j];
+    		}
+    	for(int i=0;i<2;i++)
+    		for (int j = 0; j < 2; ++j)
+    		{
+    			x[i][j]=temp[i][j]%100;
+    			
+    		}
+		
+		n=n/2;
+		
 	}
-	cout<<res[1][0]<<endl;
+	cout<<y[1][0]<<endl;
 	int stop = clock();
 	cout << "time: " << (stop-start)/double(CLOCKS_PER_SEC)*1000 << endl;	
 }

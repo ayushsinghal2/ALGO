b8aa29983803c42a14675a0de996b851a9da5dad
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-08-15 23:33:03 +0530

    INITIAL

--- fibsquare.cpp
+++ fibsquare.cpp
@@ -6,12 +6,12 @@ int main()
 {
 	int start = clock();
 	int a[2][2] = {{1,1},{1,0}};
-	int res[2][2] = {1,1,1,0};
+	int res[2][2] = {1,1,1,1};
 	long long int n;
 	cin>>n;
 	while(n>0)
 	{
-		if((n%2)!=0)
+		if((n%2)==0)
 		{
 			int D[2][2];
 			for(int R=0;R<2;R++)
@@ -25,27 +25,28 @@ int main()
     		for (int j = 0; j < 2; ++j)
     		{
     			res[i][j]=D[i][j]%100;
+    			a[i][j]=res[i][j];
     		}
 		}
 		else 
 		{
 			int D[2][2];
 			for(int R=0;R<2;R++)
-   			for(int C=0;C<2;C++)
-   			{
-      			D[R][C]=0;
-      			for(int T=0;T<2;T++)
-        			D[R][C]+=res[R][T]*a[T][C];
-    		}
-    	for(int i=0;i<2;i++)
-    		for (int j = 0; j < 2; ++j)
-    		{
-    			res[i][j]=D[i][j]%100;
-    		}
+   				for(int C=0;C<2;C++)
+   				{
+      				D[R][C]=0;
+      				for(int T=0;T<2;T++)
+        				D[R][C]+=res[R][T]*a[T][C];
+    			}
+    		for(int i=0;i<2;i++)
+    			for (int j = 0; j < 2; ++j)
+    			{
+    				res[i][j]=D[i][j]%100;
+    			}
 		}
 		n=n/2;	
 	}
-	cout<<res[0][1]<<endl;
+	cout<<res[1][0]<<endl;
 	int stop = clock();
 	cout << "time: " << (stop-start)/double(CLOCKS_PER_SEC)*1000 << endl;	
 }
--- period.cpp
+++ period.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
-#
+#include <fstream>
+#include <ctime>
+using namespace std;
+void main()
+{
+	fstream o;
+	o.open("period.bin",ios::out | ios::app | ios::binary);
+	char arr[1000];
+	for (int i = 0; i < 100; ++i) ////can be modified for all numbers...
+	{
+		
+	}
+}

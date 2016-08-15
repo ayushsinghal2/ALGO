fe9defcf56817fbe4bb8d0944c48f1f9931a9c15
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-08-16 01:14:45 +0530

    check the period

---
+++ fibmodn/periodcheck.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	fstream f;
+	int x;
+	f.open("period.bin",ios::in | ios::binary);
+	while(!(f.eof()))
+	{
+		f.read((char*)&x,sizeof(x));
+		cout<<x<<endl;
+	}
+}

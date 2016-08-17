161e4e3f2d70cb5d31027130765f88dc8671b834
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-08-17 23:45:39 +0530

    random number file

---
+++ fibmodn/random.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int a ;
+	fstream o;
+	o.open("number.bin",ios::out |  ios::binary);
+	for (int i = 0; i < 1000000; ++i)
+	{
+		a = (rand()%10);
+		
+		o.write((char *)&a,sizeof(a));
+	}
+	o.close();
+}

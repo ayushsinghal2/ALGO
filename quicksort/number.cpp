e4ecfbc5b7c71ab185088f6ee231c479ebccc16e
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-08-22 18:55:39 +0530

    rank with fixed pivot

---
+++ quicksort/number.cpp
@@ -0,0 +1,47 @@
+// find the rank of the given number
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int  partition(int a[], int low, int high )
+{//partition
+	int pivot = a[low];
+	int left =low;
+	int right = high;
+	while(left<=right)
+	{
+		while(a[left]<=pivot)
+			{left++;}
+		while(a[right]>pivot)
+			{right--;}
+		if(left<right)
+		{
+			/////swapping
+			int temp = a[left];
+			a[left]=a[right];
+			a[right]=temp;
+		}
+	}
+	a[low]=a[right];
+	a[right]=pivot;
+	return right;
+	
+}
+int rank (int a[] , int i , int j ,int r )
+{
+	int k = partition(a,i,j);
+	if (r==j-k+1)
+		return a[k];
+	else if (r<j-k+1)
+		rank(a,k+1,j,r);
+	else
+		rank(a,i,k,r-j+k);
+}
+int main()
+{
+	int a[5] = {5,3,2,1,6};
+	cout<<"Enter the rank of the number (1-5)";
+	int number;
+	cin>>number;
+	int ff = rank(a,0,4,number);
+	cout<<ff<<endl;
+}

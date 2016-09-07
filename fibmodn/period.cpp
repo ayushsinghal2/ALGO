25469d14b4fe9e6e5119ecf57ac3f8ff71182447
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-09-07 18:32:14 +0530

    period as input

--- fibmodn/period.cpp
+++ fibmodn/period.cpp
@@ -7,9 +7,11 @@ int main()
 	fstream o;
 	o.open("period.bin",ios::out | ios::app | ios::binary);
 	int f=0,s=1,next;
-	for (int n = 1; n <=(6*100)+1; ++n) ////can be modified for all numbers...
+    int nooo;
+    cin>>nooo;
+	for (int n = 1; n <=(6*nooo)+1; ++n) ////can be modified for all numbers...
 	{
-    	next=(f+s)%100;
+    	next=(f+s)%nooo;
     	f=s;
     	s=next;
     	o.write((char*)&next,sizeof(next));

8c9c62de0f1dd61d9d6e7b7dbef4aec3d6b1bf19
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-10-19 19:59:43 +0530

    Cleaning up

--- hashing/perfect.cpp
+++ hashing/perfect.cpp
@@ -3,19 +3,20 @@
 #include <cstdlib>
 #include <ctime> 
 #include <cmath>
+#include <list>
 using namespace std;
 long int hash(int a ,int x ,int b, int p, int m)
 {
 	return (((a*x)+b) % p) % m;
 }
 class ha
-{
+{	
+public:
 	int a;
 	int b;
 	int p;
 	int m;
 	int* numbers;
-public:
 	ha()
 	{
 		a=b=p=m=0;
@@ -26,6 +27,7 @@ int main()
 {
 	int no [10]={1,2,3,4,5,6,7,8,9,10};
 	int ni[20]={0};
+	int* 
 	int a,b,p,m;
 	srand (time(NULL));
 	a = rand()%10+1;
@@ -36,12 +38,14 @@ int main()
 	for (int i = 0; i < 10; ++i) //change generic
 	{
 		int c = hash(a,no[i],b,p,m);
-		if(ni[c]>0)
+		if(ni[c]==0)
 		{
 			ni[c]++;
 		}
 		else
-			ni[c]=1;
+		{
+
+		}
 	}
 	int count=0;
 	for (int i = 0; i < m; ++i) 
@@ -51,12 +55,19 @@ int main()
 	if(count<m)
 	{
 		ha* ht = (ha*) calloc(20,sizeof(ha));
-		for (int i = 0; i < m; ++i) 
+		for (int i = 0; i < 10; ++i) 
 		{
-			if(ni[i]==1)
+			int c = hash(a,no[i],b,p,m);
+			if(ht[i].numbers == NULL)
 			{
-				ht[i]->numbers = new int;
-				*(ht[i]->numbers) = 2500; 
+					ht[i].numbers = new int[ni[c]*ni[c]];
+					srand (time(NULL));
+					ht[i].a = rand()%10+1;
+					ht[i].b = rand()%997;
+					ht[i].p = 997;
+					ht[i].m = ni[c]*ni[c];
+					int d = hash(ht[i].a,no[i],ht[i].b,ht[i].p,ht[i].m);
+
 			}
 
 		}

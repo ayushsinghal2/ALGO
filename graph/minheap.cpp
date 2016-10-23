59d321566839d0442ceef3bc4da94d603e2c43da
ayushsinghal2 <ayush.singhal0294@gmail.com>
2016-10-23 18:43:26 +0530

    Traversal using minheap

--- graph/minheap.cpp
+++ graph/minheap.cpp
@@ -3,25 +3,37 @@
 #include <cstdlib>
 #include <bits/stdc++.h>
 using namespace std;
+class node
+{
+public:
+	int no;
+	int pri;
+};
 class graph
 {
 public:
 	int status[5];        //0 for green 1 for yellow 2 for blue....
 	int prev [5];         //previous node ...... (1,2,....)
-	int am [5][5];        //change generic...
+	int am [5][5]; 
+	int weight [5][5];       //change generic...
 	graph()
 	{
-		int temp [] [5] ={0,1,0,1,0,
-			  0,0,1,1,0,
+		//temp contains weight of all edges 0 implies no edge is present...
+		     int temp [] [5] ={0,3,0,4,0,
+			  0,0,9,2,0,
 			  0,0,0,0,0,
-			  0,0,0,0,1,
-			  0,1,1,0,0	
+			  0,0,0,0,6,
+			  0,3,1,0,0	
 			 }; 
 		for (int i = 0; i < 5; ++i)
 		{
 			for (int j = 0; j< 5; ++j)
 			{
-				am[i][j] = temp[i][j];
+				weight[i][j] = temp[i][j];
+				if(temp[i][j]!=0)
+					am[i][j]=1;
+				else
+					am[i][j]=0;
 			}
 		}
 		int ts[5] = {0,0,0,0,0};//change generic...
@@ -40,18 +52,63 @@ public:
 		}
 	}
 };
+class myComparator 
+{
+public:
+	int operator()(const node& p1, const node& p2)
+	{
+		return p1.pri > p2.pri;
+	}
+};
 class hfs
 {
-	 priority_queue <int, vector<int>, greater<int> > pq; //minheap.... 
-	 //(priority_queue <datatype , vector <type> , comparator <datatype>>)
-	 //greater is an inbuilt function in stl....
-	 //you can have your own comparator ...
-
+	priority_queue <node, vector<node>, myComparator > pq; //minheap.... 
+	//(priority_queue <datatype , vector <type> , comparator <datatype>>)
+	//greater is an inbuilt function in stl....
+	//you can have your own comparator ...
+	node n [5];
+public:
+	void visit(graph g)
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			n[i].no = i;
+			n[i].pri = 0; 
+		}
+		pq.push(n[0]);
+		g.status[0] = 1;
+		while(!pq.empty())
+		{
+			node temp = pq.top();
+			pq.pop();
+			int i = temp.no;
+			g.status[i] = 2;
+			for (int j = 0; j < 5; ++j)
+			{
+				if(g.status[j]==0 && g.am[i][j]==1) //adding all the neighbours to the heap...
+				{
+					n[j].pri = n[i].pri + g.weight[i][j];
+					g.prev[j] = i;
+					g.status[j]=1;
+					pq.push(n[j]);
+				}
+				else if(g.status[j]==1 &&g.am[i][j]==1) //relaxing the weights ....
+				{
+					if(n[j].pri>n[i].pri+g.weight[i][j])
+					{
+						n[j].pri = n[i].pri + g.weight[i][j];
+						g.prev[j] = i;
+					}
+				}
+			}
+		}
+		g.displaytr();	
+	}
 	
 };
 int main()
 {
-	bfs b;
+	hfs b;
 	graph h;
 	b.visit(h);
 }

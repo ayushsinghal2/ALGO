3bc97a7f52e1ffe4c2ab573cd386c16b52b8bb2f
ayushsinghal2 <ayushsinghal0294@gmail.com>
2016-11-13 01:29:17 +0530

    dfs with time

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -9,6 +9,8 @@ public:
 	int status[5];        //0 for green 1 for yellow 2 for blue....
 	int prev [5];         //previous node ...... (1,2,....)
 	int am [5][5];        //change generic...
+	int st[5]; //start time
+	int ft[5]; //finish time
 	graph()
 	{
 		int temp [] [5] ={0,1,0,1,0,
@@ -30,13 +32,16 @@ public:
 		{
 			status[i] = ts[i];
 			prev[i]=tp[i];
+			st[i] = tp[i];
+			ft[i] = tp[i];
 		}
 	}
 	void displaytr()
 	{
 		for (int i = 0; i < 5; ++i)
 		{
-			cout<<"Status = "<<status[i]<<endl<<" Previous Node = "<<prev[i]<<endl;
+			cout<<"Previous Node = "<<prev[i]<<endl<<"Discover Time = " <<st[i]<<endl
+			<<"End Time = "<<ft[i]<<endl;
 		}
 	}
 };
@@ -46,20 +51,30 @@ class dfs
 public:
 	void visit (graph g)
 	{
+		int tim = 1;
 		ll.push_back(0);
 		g.status[0]=1;
+		g.st[0] = tim;
 		while(!ll.empty())
 		{
 			int i = ll.back();
 			ll.pop_back();
 			g.status[i]=2;
+			tim++;
 			for (int j = 0; j < 5; ++j)
 			{
+				
 				if(g.am[i][j]==1 && g.status[j]!=2) //adding all neighbours to the stack...
 				{
 					ll.push_back(j);
 					g.prev[j] = i;
 					g.status[j] = 1; 
+					g.st[j] = tim;
+				}
+				if (g.status[j]==2)
+				{
+					if(g.ft[j]==-1)
+						{g.ft[j]=tim;}
 				}
 			}
 		}
